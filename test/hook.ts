70d9f57dfeaefcb84289ef7e3ed1d590b0266d1e
skt-t1-byungi <tiniwz@gmail.com>
2019-03-15 23:32:41 +0900

    add test

--- test/hook.ts
+++ test/hook.ts
@@ -1,5 +1,6 @@
 import test from 'ava'
 import { renderHook, act } from 'react-hooks-testing-library'
+import { createElement, Fragment } from 'react'
 import createStore from '../src'
 import './_browser'
 
@@ -16,4 +17,29 @@ test('render, update', t => {
     })
 
     t.deepEqual(result.current, { n: 1 })
+})
+
+test('rendering should not be done twice in nested components.', t => {
+    const store = createStore({ val: 'hello' })
+    let calls = 0
+
+    const wrapper = ({ children }: any) => {
+        store.useStore(s => s.val)
+        calls++
+        return createElement(Fragment, undefined, children)
+    }
+
+    renderHook(() => {
+        calls++
+        return store.useStore(s => s.val)
+    }, { wrapper : wrapper as any })
+
+    t.is(calls, 2)
+
+    act(() => {
+        store.update(s => {
+            s.val = 'world'
+        })
+    })
+    t.is(calls, 4)
 })
