4646b8cea50734439928a27ac8de328110ab838d
byungi <tiniwz@gmail.com>
2020-10-13 11:22:43 +0900

    lint

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,15 @@ import { Dispatch, useCallback, useLayoutEffect, useReducer, useRef } from 'reac
 import equal from 'dequal'
 import produce, { Draft } from 'immer'
 
-type Listener<T> = (state: T) => void
-type Mutator<T> = (state: Draft<T>) => void
-type Selector<T, Result> = (state: T) => Result
+type Listener<T> = (state:T) => void
+type Mutator<T> = (state:Draft<T>) => void
+type Selector<T, Result> = (state:T) => Result
 
 export class Store<T> {
-    private _state: T
-    private _listeners: Array<Listener<T>> = []
+    private _state:T
+    private _listeners:Array<Listener<T>> = []
 
-    constructor (initialState: T) {
+    constructor (initialState:T) {
         this._state = Object(initialState)
         this.getState = this.getState.bind(this)
         this.update = this.update.bind(this)
@@ -18,30 +18,30 @@ export class Store<T> {
         this.useStore = this.useStore.bind(this)
     }
 
-    getState (): Readonly<T> {
+    getState ():Readonly<T> {
         return this._state
     }
 
-    update (mutate: Mutator<T>) {
+    update (mutate:Mutator<T>) {
         const nextState = produce(this._state, draft => mutate(draft))
         if (this._state !== (this._state = nextState)) {
             this._listeners.forEach(fn => fn(this._state))
         }
     }
 
-    subscribe (listener: Listener<T>) {
+    subscribe (listener:Listener<T>) {
         this._listeners.push(listener)
         return () => void this._listeners.splice(this._listeners.indexOf(listener), 1)
     }
 
     useStore<F extends Selector<T, any>= Selector<T, T>> (
-        selector: F = passThrough as any,
-        deps: any[] = []
-    ): ReturnType<F> {
+        selector:F = passThrough as any,
+        deps:any[] = []
+    ):ReturnType<F> {
         const currSelector = useCallback(selector, deps)
         const selectorRef = useRef(selector)
         const stateRef = useRef<ReturnType<F>>()
-        const forceUpdate: Dispatch<void> = useReducer(() => ({}), {})[1]
+        const forceUpdate:Dispatch<void> = useReducer(() => ({}), {})[1]
 
         useLayoutEffect(() => { selectorRef.current = currSelector })
 
@@ -65,9 +65,9 @@ export class Store<T> {
     }
 }
 
-function passThrough<T> (val: T) { return val }
+function passThrough<T> (val:T) { return val }
 
-export function createStore<T> (initialState: T) {
+export function createStore<T> (initialState:T) {
     return new Store(initialState)
 }
 
